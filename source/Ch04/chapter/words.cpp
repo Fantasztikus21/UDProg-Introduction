5f0f5122141246ed25ed0bdf20047ce3673065f4
gbalázs <gebri08@gmail.com>
2021-12-06 18:32:10 +0000

    Második rész feltöltve

---
+++ source/Ch04/chapter/words.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+int main(){
+
+	vector<string> words;
+
+	for (string temp; cin >> temp; )
+		words.push_back(temp);
+
+	cout << "Number of words: " << words.size() << endl;
+
+	sort(words);
+
+	for(auto word : words)
+		cout << word << endl;
+
+	return 0;
+}
